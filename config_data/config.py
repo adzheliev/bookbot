2efd65051c7f9a025538b5ae23203a1c8a581bc7
Алан Джелиев <djalan84@mail.ru>
2024-09-03 10:43:24 +0300

    first commit

---
+++ config_data/config.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+import os
+
+# from environs import Env
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+@dataclass
+class TgBot:
+    token: str  # Токен для доступа к телеграм-боту
+    admin_ids: list[int]  # Список id администраторов бота
+
+
+@dataclass
+class Config:
+    tg_bot: TgBot
+
+
+# Создаем функцию, которая будет читать файл .env и возвращать
+# экземпляр класса Config с заполненными полями token и admin_ids
+def load_config(path: str | None = None) -> Config:
+    # env = Env()
+    # env.read_env(path)
+    return Config(
+        tg_bot=TgBot(
+            token=os.environ.get('BOT_TOKEN'),
+            admin_ids=list(map(int, os.environ.get('ADMIN_IDS')))
+        )
+    )
